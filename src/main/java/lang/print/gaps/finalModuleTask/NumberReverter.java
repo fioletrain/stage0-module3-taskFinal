d828de4c47e1f44adff01ac8eafa9d457a723fe4
Svitlana D <svetk2801@gmail.com>
2023-01-07 18:59:19 +0200

    Module 3 final

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,12 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int result = 0;
+        while (number != 0) {
+            result = result * 10;
+            result = result + number % 10;
+            number = number / 10;
+        }
+        System.out.println(result);
     }
 }
